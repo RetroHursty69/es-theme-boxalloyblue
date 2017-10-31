2e92d9c33f66f19c25b6099ee3a6afd25d181fd8
RetroHursty69 <hurstyd@gmail.com>
2017-10-31 05:50:16 -0500

    Version 1.0

---
+++ README.md
@@ -0,0 +1,24 @@
+# es-theme-boxalloy
+
+OVERVIEW
+
+This ZIP package contains the Box Alloy theme that is to be used within Emulation Station.
+
+**NOTE**
+
+You do not always have to put Emulation Station themes in the standard /etc/emulationstation/themes location.
+
+Emulation Station provides additional support for placing them into a different folder.  The benefit of using this
+different folder is that the default user "pi" has full read/write access.  You no longer have to be user "root" to
+copy over files or swap out files.  
+
+By default, this new "themes" folder does not exist, so simply create it.
+
+/opt/retropie/configs/all/emulationstation/themes
+
+**NOTE**
+You can also access this same folder by navigating this hidden folder
+   
+/home/pi/.emulationstation
+
+
---
+++ temp/move.bat
@@ -0,0 +1,131 @@
+move "All Games.png" ..\auto-allgames\_inc\system.png
+move "Amiga CD32.png" ..\amigacd32\_inc\system.png
+move "Amstrad CPC.png" ..\amstradcpc\_inc\system.png
+move "Apple II.png" ..\apple2\_inc\system.png
+move "Arcade.png" ..\arcade\_inc\system.png
+move "Atari 2600.png" ..\atari2600\_inc\system.png
+move "Atari 5200.png" ..\atari5200\_inc\system.png
+move "Atari 7800.png" ..\atari7800\_inc\system.png
+move "Atari 800.png" ..\folder\_inc\system.png
+move "Atari Classics.png" ..\atari\_inc\system.png
+move "Atari Jaguar.png" ..\atarijaguar\_inc\system.png
+move "Atari Lynx.png" ..\atarilynx\_inc\system.png
+move "Atari ST.png" ..\atarist\_inc\system.png
+move "Bandai WonderSwan Color.png" ..\wonderswancolor\_inc\system.png
+move "Bandai WonderSwan.png" ..\wonderswan\_inc\system.png
+move "Batman Collection.png" ..\batman\_inc\system.png
+move "Beatem Up Collection.png" ..\beatemup\_inc\system.png
+move "Capcom Classics.png" ..\capcom\_inc\system.png
+move "Capcom Play System II.png" ..\cps2\_inc\system.png
+move "Capcom Play System III.png" ..\cps3\_inc\system.png
+move "Capcom Play System.png" ..\cps1\_inc\system.png
+move "Castlevania Collection.png" ..\castlevania\_inc\system.png
+move "Colecovision.png" ..\colecovision\_inc\system.png
+move "Commodore 64.png" ..\c64\_inc\system.png
+move "Commodore Amiga.png" ..\amiga\_inc\system.png
+move "Commodore VIC-20.png" ..\vic20\_inc\system.png
+move "Custom Collections.png" ..\custom-collections\_inc\system.png
+move "Daphne.png" ..\daphne\_inc\system.png
+move "Data East Classics.png" ..\dataeast\_inc\system.png
+move "Disney Collection.png" ..\disney\_inc\system.png
+move "Donkey Kong Collection.png" ..\donkeykong\_inc\system.png
+move "Double Dragon Collection.png" ..\doubledragon\_inc\system.png
+move "Favorites.png" ..\auto-favorites\_inc\system.png
+move "Final Burn Alpha.png" ..\fba\_inc\system.png
+move "Final Fantasy Collection.png" ..\finalfantasy\_inc\system.png
+move "Frogger Collection.png" ..\frogger\_inc\system.png
+move "GCE Vectrex.png" ..\vectrex\_inc\system.png
+move "Irem Classics.png" ..\irem\_inc\system.png
+move "Kodi.png" ..\kodi\_inc\system.png
+move "KOF Collection.png" ..\kof\_inc\system.png
+move "Konami Classics.png" ..\konami\_inc\system.png
+move "Last Played.png" ..\auto-lastplayed\_inc\system.png
+move "Lego Collection.png" ..\lego\_inc\system.png
+move "Light Gun Collection.png" ..\lightgun\_inc\system.png
+move "Magnavox Odyssey 2.png" ..\odyssey2\_inc\system.png
+move "Mame.png" ..\mame\_inc\system.png
+move "Mario Collection.png" ..\mario\_inc\system.png
+move "Mattel Intellivision.png" ..\intellivision\_inc\system.png
+move "Mega Man Collection.png" ..\megaman\_inc\system.png
+move "Metal Slug Collection.png" ..\metalslug\_inc\system.png
+move "Metroid Collection.png" ..\metroid\_inc\system.png
+move "Microsoft MSX 2.png" ..\msx2\_inc\system.png
+move "Microsoft MSX.png" ..\msx\_inc\system.png
+move "Mortal Kombat Collection.png" ..\mortalkombat\_inc\system.png
+move "Namco Classics.png" ..\namco\_inc\system.png
+copy "NEC PC Engine-CD.png" ..\pcenginecd\_inc\system.png
+move "NEC PC Engine-CD.png" ..\pce-cd\_inc\system.png
+move "NEC PC Engine.png" ..\pcengine\_inc\system.png
+copy "NEC SuperGrafx.png" ..\sgfx\_inc_system.png
+move "NEC SuperGrafx.png" ..\supergrafx\_inc\system.png
+move "NEC TurboGrafx-16.png" ..\tg16\_inc\system.png
+move "NEC TurboGrafx-CD.png" ..\tg16cd\_inc\system.png
+move "Neo Geo Pocket Color.png" ..\ngpc\_inc\system.png
+move "Neo Geo Pocket.png" ..\ngp\_inc\system.png
+move "Neo Geo.png" ..\neogeo\_inc\system.png
+move "Nintendo 64.png" ..\n64\_inc\system.png
+move "Nintendo BSX Satellaview.png" ..\nintendobsx\_inc\system.png
+move "Nintendo Classics.png" ..\nintendo\_inc\system.png
+move "Nintendo DS.png" ..\nds\_inc\system.png
+move "Nintendo Famicom Disk System.png" ..\fds\_inc\system.png
+move "Nintendo Famicom.png" ..\famicom\_inc\system.png
+move "Nintendo Game and Watch.png" ..\gameandwatch\_inc\system.png
+move "Nintendo Game Boy Advance.png" ..\gba\_inc\system.png
+move "Nintendo Game Boy Color.png" ..\gbc\_inc\system.png
+move "Nintendo Game Boy.png" ..\gb\_inc\system.png
+move "Nintendo Game Cube.png" ..\gc\_inc\system.png
+move "Nintendo Super Famicom.png" ..\sfc\_inc\system.png
+move "Nintendo Virtual Boy.png" ..\virtualboy\_inc\system.png
+move "Nintendo Wii.png" ..\wii\_inc\system.png
+move "Nintendo WiiU.png" ..\wiiu\_inc\system.png
+move "Oric.png" ..\oric\_inc\system.png
+move "Pacman Collection.png" ..\pacman\_inc\system.png
+move "Panasonic 3DO.png" ..\3do\_inc\system.png
+move "PC Games.png" ..\pc\_inc\system.png
+move "Philips Videopac.png" ..\videopac\_inc\system.png
+move "Pinball Collection.png" ..\pinball\_inc\system.png
+move "Pixel Desktop.png" ..\pixel\_inc\system.png
+move "Pokemon Collection.png" ..\pokemon\_inc\system.png
+move "Ports.png" ..\ports\_inc\system.png
+move "Residual VM.png" ..\residualvm\_inc\system.png
+move "RetroPie Setup.png" ..\retropie\_inc\system.png
+move "ScummVm.png" ..\scummvm\_inc\system.png
+move "Sega 32X.png" ..\sega32x\_inc\system.png
+move "Sega CD.png" ..\segacd\_inc\system.png
+move "Sega Classics.png" ..\sega\_inc\system.png
+move "Sega Dreamcast.png" ..\dreamcast\_inc\system.png
+move "Sega Game Gear.png" ..\gamegear\_inc\system.png
+move "Sega Genesis.png" ..\genesis\_inc\system.png
+move "Sega Mark III.png" ..\markiii\_inc\system.png
+move "Sega Master System Tectoy.png" ..\tectoy\_inc\system.png
+move "Sega Master System.png" ..\mastersystem\_inc\system.png
+move "Sega Mega Drive Japan.png" ..\megadrive-japan\_inc\system.png
+move "Sega Mega Drive.png" ..\megadrive\_inc\system.png
+move "Sega Mega-CD.png" ..\megacd\_inc\system.png
+move "Sega Saturn.png" ..\saturn\_inc\system.png
+move "Sega SC-3000.png" ..\sc-3000\_inc\system.png
+move "Sega SG-1000.png" ..\sg-1000\_inc\system.png
+move "Shmups.png" ..\shmups\_inc\system.png
+move "Simpsons Collection.png" ..\simpsons\_inc\system.png
+move "Sinclair ZX Spectrum.png" ..\zxspectrum\_inc\system.png
+move "SNK Classics.png" ..\snk\_inc\system.png
+move "Sonic Collection.png" ..\sonic\_inc\system.png
+move "Sony PlayStation 2.png" ..\ps2\_inc\system.png
+move "Sony PlayStation.png" ..\psx\_inc\system.png
+move "Sony PSP Minis.png" ..\pspminis\_inc\system.png
+move "Sony PSP.png" ..\psp\_inc\system.png
+move "Space Invaders Collection.png" ..\spaceinvaders\_inc\system.png
+move "Spiderman Collection.png" ..\spiderman\_inc\system.png
+move "Star Wars Collection.png" ..\starwars\_inc\system.png
+move "Steam.png" ..\steam\_inc\system.png
+move "Strett Fighter Collection.png" ..\streetfighter\_inc\system.png
+move "Sufami Turbo.png" ..\sufami\_inc\system.png
+move "Super Nintendo Entertainment System.png" ..\snes\_inc\system.png
+move "Taito Classics.png" ..\taito\_inc\system.png
+move "Texas TI-994A.png" ..\ti99\_inc\system.png
+move "TMNT Collection.png" ..\tmnt\_inc\system.png
+move "Trackball Collection.png" ..\trackball\_inc\system.png
+move "Williams Classics.png" ..\williams\_inc\system.png
+move "Wonder Boy Collection.png" ..\wonderboy\_inc\system.png
+move "Zelda Collection.png" ..\zelda\_inc\system.png
+move "ZMachine.png" ..\zmachine\_inc\system.png
